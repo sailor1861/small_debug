4d51bdf5ac995c6339835416170433f2a9799e77
yedarong <yedarong@autohome.com.cn>
2017-10-14 16:37:50 +0800

    UPDATE

--- Android/DevSample/buildSrc/src/main/groovy/net/wequick/gradle/RootPlugin.groovy
+++ Android/DevSample/buildSrc/src/main/groovy/net/wequick/gradle/RootPlugin.groovy
@@ -126,6 +126,7 @@ class RootPlugin extends BasePlugin {
                 }
 
                 // 配置buildLib.doLast，编译插件后的后处理任务
+                // todo: hostPlugin，libPlugin内实现是否均可以呢？
                 if (it.hasProperty('buildLib')) {
                     Log.success "project($it.project) hasProperty buildLib"
                     it.small.buildIndex = ++rootExt.libCount
