9ef5012b7a7b86f3f21517332ecb090543932658
yedarong <yedarong@autohome.com.cn>
2017-11-20 19:47:04 +0800

    update clear.bat for clean project

---
+++ Android/DevSample/clear.bat
@@ -0,0 +1,13 @@
+
+@rem ##########################################################################
+@rem
+@rem  Gradle startup script for Windows
+@rem
+@rem ##########################################################################
+
+@rem Set local scope for the variables with windows NT shell
+rm -rf publicCache
+
+rm -rf ../Sample/app/destLibs
+
+ 
