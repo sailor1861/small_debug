f101882d578c6999f14920fd08056de612b77120
yedarong <yedarong@autohome.com.cn>
2017-11-20 11:02:58 +0800

    新增TestCase： app.detail插件 对 lib.style公共插件 的Java代码， R.java, XML资源的引用

--- Android/Sample/app.detail/src/main/java/net/wequick/example/small/app/detail/MainActivity.java
+++ Android/Sample/app.detail/src/main/java/net/wequick/example/small/app/detail/MainActivity.java
@@ -1,14 +1,19 @@
 package net.wequick.example.small.app.detail;
 
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.example.mysmall.lib.style.StyleTestJava;
 
 import net.wequick.small.Small;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "app.detail";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
                 tvFrom.setText("-- Greet from " + from);
             }
         }
+
+        testLibStylePlugin();
     }
 
     @Override
@@ -44,5 +51,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outState) {
         outState.putString("Hello", "Small");
         super.onSaveInstanceState(outState);
+    }
+
+    /**
+     * 验证App插件对lib插件的访问：包括Java代码，R.java资源ID
+     */
+    private void testLibStylePlugin() {
+        // App插件访问lib插件的java类
+        new StyleTestJava().add(1, 14);
+
+        // App插件在代码中访问lib插件的R.java: 要求资源ID固定
+        Log.i(TAG, "R.color.colorLight: " + Integer.toHexString(R.color.colorLight) + "; colorPrimaryDark: " + Integer.toHexString(R.color.colorPrimaryDark));
     }
 }
